4b0c935c5ae5d4e86b83b179ff936fa1dece1746
Jan-Eric Hellenberg <janeric.hellenberg@gmail.com>
2022-01-11 14:26:45 +0100

    Add py_webauthn project files

---
+++ py_webauthn/CHANGELOG.md
@@ -0,0 +1,48 @@
+# Changelog
+
+## v1.2.1
+
+**Changes:**
+
+- Clarify `credential` docstring for `verify_authentication_response()`
+
+
+## v1.2.0
+
+**Changes:**
+
+- Switched from Pydantic to the combination of **attrs + cattrs**. This achieves more-Pythonic library behavior when used in a project alongside other third-party packages that use subclasses of `bytes` to represent such values as credential IDs and public keys.
+
+
+## v1.1.0
+
+**Changes:**
+
+- Fixed SafetyNet attestation statement verification failing due to server time drift
+- Added py.typed file to indicate type information is present (PEP-561)
+
+
+## v1.0.1
+
+**Changes:**
+
+- Fixed SafetyNet unit test failing due to expired x5c certs (see PR #99)
+
+
+## v1.0.0 - Like a whole new library...
+
+This preview release of the revitalized py_webauthn library features an entirely new API, as well as support for all attestation statement formats included in L2 of the WebAuthn spec:
+
+- **Packed**
+- **TPM**
+- **Android Key**
+- **Android SafetyNet**
+- **FIDO U2F**
+- **Apple**
+- **None**
+
+Practical examples are included in the **examples/** directory to serve as a primary reference for now on how to use the new library functionality.
+
+**Changes:**
+
+- Everything. The entire package was replaced with a new library with a new API. Check it out :rocket:
---
+++ py_webauthn/setup.py
@@ -0,0 +1,58 @@
+import codecs
+import os
+import re
+
+from setuptools import setup, find_packages
+
+
+HERE = os.path.abspath(os.path.dirname(__file__))
+
+
+def read(*parts):
+    with codecs.open(os.path.join(HERE, *parts), 'r') as fp:
+        return fp.read()
+
+
+def find_version(*file_paths):
+    version_file = read(*file_paths)
+    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]",
+                              version_file, re.M)
+    if version_match:
+        return version_match.group(1)
+    raise RuntimeError('Unable to find version string.')
+
+
+LONG_DESCRIPTION = read('README.md')
+VERSION = find_version('webauthn', '__init__.py')
+
+
+setup(
+    name='webauthn',
+    packages=find_packages(exclude=["tests"]),
+    include_package_data=True,
+    package_data={"webauthn": ["py.typed"]},
+    version=VERSION,
+    description='Pythonic WebAuthn',
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type='text/markdown',
+    keywords='webauthn fido2',
+    author='Duo Labs',
+    author_email='labs@duo.com',
+    url='https://github.com/duo-labs/py_webauthn',
+    download_url='https://github.com/duo-labs/py_webauthn/archive/{}.tar.gz'.format(VERSION),
+    license='BSD',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python :: 3'
+    ],
+    install_requires=[
+        'asn1crypto>=0.24.0',
+        'cbor2>=4.0.1',
+        'cryptography>=3.4.7',
+        'attrs>=21.2.0',
+        'cattrs>=1.8.0',
+        'pyOpenSSL>=20.0.1',
+    ]
+)
