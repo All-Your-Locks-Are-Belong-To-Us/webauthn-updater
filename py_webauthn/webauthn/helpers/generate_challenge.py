27d5cf70e84e2f3fb7ed73c0ed04479ede9523a4
Felix Gohla <felix.gohla@student.hpi.de>
2022-02-12 13:54:25 +0100

    Remove Local Copy Of py_webauthn

--- py_webauthn/webauthn/helpers/generate_challenge.py
+++
@@ -1,8 +0,0 @@
-import secrets
-
-
-def generate_challenge(length: int = 64) -> bytes:
-    """
-    Generate a random authenticator challenge
-    """
-    return secrets.token_bytes(length)
--- py_webauthn/webauthn/helpers/tpm/__init__.py
+++
@@ -1,2 +0,0 @@
-from .parse_cert_info import parse_cert_info  # noqa: F401
-from .parse_pub_area import parse_pub_area  # noqa: F401
