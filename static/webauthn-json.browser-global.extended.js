107dafba9277b62e32f272afa69f3c324127f109
Jan-Eric Hellenberg <janeric.hellenberg@gmail.com>
2022-02-18 10:59:14 +0100

    Use browser-global instead of cjs distribution version of webauthn-json

--- static/webauthn-json.browser-global.extended.js
+++ static/webauthn-json.browser-global.extended.js
@@ -1,25 +1,8 @@
 var __defProp = Object.defineProperty;
-var __getOwnPropDesc = Object.getOwnPropertyDescriptor;
-var __getOwnPropNames = Object.getOwnPropertyNames;
-var __hasOwnProp = Object.prototype.hasOwnProperty;
-var __markAsModule = (target) => __defProp(target, "__esModule", { value: true });
 var __export = (target, all) => {
   for (var name in all)
     __defProp(target, name, { get: all[name], enumerable: true });
 };
-var __reExport = (target, module2, copyDefault, desc) => {
-  if (module2 && typeof module2 === "object" || typeof module2 === "function") {
-    for (let key of __getOwnPropNames(module2))
-      if (!__hasOwnProp.call(target, key) && (copyDefault || key !== "default"))
-        __defProp(target, key, { get: () => module2[key], enumerable: !(desc = __getOwnPropDesc(module2, key)) || desc.enumerable });
-  }
-  return target;
-};
-var __toCommonJS = /* @__PURE__ */ ((cache) => {
-  return (module2, temp) => {
-    return cache && cache.get(module2) || (temp = __reExport(__markAsModule({}), module2, 1), cache && cache.set(module2, temp), temp);
-  };
-})(typeof WeakMap !== "undefined" ? /* @__PURE__ */ new WeakMap() : 0);
 var __async = (__this, __arguments, generator) => {
   return new Promise((resolve, reject) => {
     var fulfilled = (value) => {
@@ -342,5 +325,7 @@ function getExtended(requestJSON) {
     return getExtendedResponseToJSON(credential);
   });
 }
-module.exports = __toCommonJS(extended_exports);
-//# sourceMappingURL=webauthn-json.extended.cjs.map
+
+// src/webauthn-json/browser-global.extended.ts
+globalThis.webauthnJSON = extended_exports;
+//# sourceMappingURL=webauthn-json.browser-global.extended.js.map
